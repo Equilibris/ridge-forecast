faa1e6b86a83d2422dc381e36f49ee9a24e01efd
HFZR2005 <100490433+HFZR2005@users.noreply.github.com>
2024-05-18 16:17:27 +0100

    - map markers now indicate activity - marker for last viewed mountain is a different colour

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,12 @@
 import React, { useCallback, useRef, useState } from "react"
 
 import MapView, { Marker } from "react-native-maps"
+import { Icon } from "react-native-paper"
+import { View, StyleSheet } from "react-native"
 import BottomSheet from "@gorhom/bottom-sheet"
 import styled from "@emotion/native"
 import { useData } from "@/data/dataProvider"
-import { Mountain } from "@/data/models"
+import { Climb, Mountain, Ski } from "@/data/models"
 import { MapModal } from "@/components/MapModal"
 import { ThemeProvider } from "@/components/ThemeProvider"
 import { useTheme } from "@emotion/react"
@@ -44,6 +46,36 @@ export default function Index() {
     )
   }, [selected, sheetRef])
 
+
+  const markerStyles = StyleSheet.create({
+    marker: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: 30,
+      height: 30,
+      borderRadius: 20,
+      borderWidth: 1,
+      borderColor: 'white',
+      shadowColor: '#000',
+      shadowOffset: { width: 0, height: 2 },
+      shadowOpacity: 0.8,
+      shadowRadius: 2,
+      elevation: 5,
+    },
+  });
+
+
+  const getIconName = (activity: Ski | Climb) => {
+    switch (activity.type) {
+      case 'ski':
+      return 'ski';
+      case 'climb':
+        return 'terrain';
+      default:
+        return 'help'
+    }
+  }
+
   return (
     <ThemeProvider>
       <Main>
@@ -58,18 +90,29 @@ export default function Index() {
         >
           {mountains.map((v) => (
             <Marker
-              key={v.id}
-              coordinate={{
-                latitude: v.pos.latitude,
-                longitude: v.pos.longitude,
-              }}
-              title={v.name}
-              tappable
-              onPress={() => {
-                setSelected(v)
-                openSheet()
-              }}
-            />
+            key={v.id}
+            coordinate={{
+              latitude: v.pos.latitude,
+              longitude: v.pos.longitude,
+            }}
+            title={v.name}
+            tappable
+            onPress={() => {
+              setSelected(v);
+              openSheet();
+            }}
+          >
+            <View style={[
+                markerStyles.marker,
+                { backgroundColor: selected && selected.id === v.id ? '#8B0000' : 'red' }
+              ]}>
+              <Icon
+                source={getIconName(v.data)}
+                size={20}
+                color={"white"}
+              />
+            </View>
+          </Marker>
           ))}
         </MapView>
       </Main>
